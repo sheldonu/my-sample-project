34ae5d1b01c7cb2f0421ddc8500162146a29b03e
sheldonu <uch20002@byui.edu>
2022-09-17 16:54:05 -0700

    first_part

---
+++ tictactoe.py
@@ -0,0 +1,18 @@
+# Tic-Tac-Toe Assignment -- Sheldon Uchytil
+
+def main():
+    Q1 = int(input("""
+1|2|3
+-+-+-
+4|5|6
+-+-+-
+7|8|9
+"""))
+
+def x_turn():
+    int(input("x's turn to choose a sqaure (1-9): "))
+
+
+
+
+main()
